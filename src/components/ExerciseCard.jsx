62c42a4ce7608f0d06e9b0da7622afd9d1e2a133
Precious Udegbue <Precious.Udegbue@sterling.ng>
2022-07-30 17:21:00 +0100

    first commit

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -7,14 +7,14 @@ const ExerciseCard = ({ exercise }) => (
     <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
     <Stack direction="row">
       <Button sx={{ ml: '21px', color: '#fff', background: '#FFA9A9', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
-        {exercise.bodyPart}
+        {exercise?.bodyPart}
       </Button>
       <Button sx={{ ml: '21px', color: '#fff', background: '#FCC757', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
         {exercise.target}
       </Button>
     </Stack>
     <Typography ml="21px" color="#000" fontWeight="bold" sx={{ fontSize: { lg: '24px', xs: '20px' } }} mt="11px" pb="10px" textTransform="capitalize">
-      {exercise.name}
+      {exercise?.name}
     </Typography>
   </Link>
 );
--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -13,7 +13,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   useEffect(() => {
     const fetchExercisesData = async () => {
       const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
-
+      console.log(bodyPartsData)
       setBodyParts(['all', ...bodyPartsData]);
     };
 
